b17383b92990375ad3277db7e9ae6ae49cabe1bb
dembot1 <148806117+dembot1@users.noreply.github.com>
2023-11-07 17:36:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,24 +6,26 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/text-to-image', async (req, res) => {
-    // Call to Random Word API
-    const randomWordResponse = await axios.get('https://random-word-api.herokuapp.com/word', {
-        headers: {
-            'X-RapidAPI-Key': '<insert-random-word-api-key-here>',
-        }
-    });
-    const text = randomWordResponse.data[0];  // Get the first word from the response
+const fetchRandomWord = async () => {
+    const response = await axios.get('https://random-word-api.herokuapp.com/word');
+    return response.data[0];
+}
 
+const fetchImage = async (text) => {
     const response = await axios.get(`https://img4me.p.rapidapi.com/?text=${text}&font=arial&fcolor=000000&size=35&type=png`, {
         headers: {
             'X-RapidAPI-Host': 'img4me.p.rapidapi.com',
             'X-RapidAPI-Key': '5af3cea84fmsh2ca76ff2be471a8p1ab760jsn8c2a4a4b7d45',
         }
     });
-    res.json({ imageUrl: response.data, text: text });
+    return response.data;
+}
+
+app.get('/text-to-image', async (req, res) => {
+    const text = await fetchRandomWord();
+    const imageUrl = await fetchImage(text);
+    res.json({ imageUrl, text});
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
